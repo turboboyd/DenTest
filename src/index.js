67c04167da2d73e313208accc8bda163ec4053eb
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-11-21 21:12:57 +0100

    add

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/DenTest">
+        <BrowserRouter basename="/PowerPulse">
           <App />
           <ToastContainer />
         </BrowserRouter>
